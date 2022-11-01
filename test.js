f2df5274ef16a2e47f364445317ab94cc094c091
Archit-Arya <73984403+Archit-Arya@users.noreply.github.com>
2022-11-01 14:34:13 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,28 @@
+document.addEventListener('mousemove',(e)=>{
+    const mouseX=e.clientX;
+    const mouseY=e.clientY;
+
+    const anchor=document.getElementById('anchor')
+    const rekt=anchor.getBoundingClientRect();
+    const anchorX=(rekt.left+rekt.width)/2;
+    const anchorY=(rekt.top+rekt.bottom)/2;
+
+    const angleDeg=angle(mouseX,mouseY,anchorX,anchorY);
+    console.log(angleDeg)
+
+    const eyes=document.querySelectorAll('.eye')
+
+    eyes.forEach(eye => {
+        //document.querySelector('.eye').style.transform = 'rotate(90deg)';
+        eye.style.transform=`rotate(${90+angleDeg}deg)`;
+        anchor.style.filter=`hue-rotate(${angleDeg}deg)`;
+    })
+})
+
+function angle(cx,cy,ex,ey){
+    const dy=ey-cy;
+    const dx=ex-cx;
+    const rad=Math.atan2(dy,dx);
+    const deg=rad*180/Math.PI;
+    return deg;
+}
